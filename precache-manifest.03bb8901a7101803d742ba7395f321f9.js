bed0eb89f9041da3fee0861010bce312e7542372
Sam Poe <rckatz@hotmail.com>
2019-11-18 20:21:55 -0500

    Updates

---
+++ precache-manifest.03bb8901a7101803d742ba7395f321f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77f5bc4df6e25de358fd21908da8f444",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "70214c75c08aebacb430",
+    "url": "/robofriends/static/css/main.2ee736fb.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "70214c75c08aebacb430",
+    "url": "/robofriends/static/js/main.bd2baee5.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
